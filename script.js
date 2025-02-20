263faa0fff20158de4f3f1ae1ec2339a7d76a94c
Ilya <ilya.saenko.06@mail.ru>
2025-02-20 12:16:25 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+﻿document.addEventListener('DOMContentLoaded', function() {
+    console.log('Страница загружена!');
+    const buttons = document.querySelectorAll('.button');
+    buttons.forEach(button => {
+        button.addEventListener('mouseover', function() {
+            this.style.backgroundColor = '#555';
+        });
+        button.addEventListener('mouseout', function() {
+            this.style.backgroundColor = '#e44d26';
+        });
+    });
+});
